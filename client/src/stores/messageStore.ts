f1c071f5093cc0f0269938491562946c8b71cfe4
Vaharsolta <vaha-22@mail.ru>
2026-07-15 11:52:37 +0300

    feat(messages): store actions updateMessage/removeMessage

--- client/src/stores/messageStore.ts
+++ client/src/stores/messageStore.ts
@@ -5,6 +5,8 @@ interface MessageState {
   messages: Message[];
   setMessages: (messages: Message[]) => void;
   addMessage: (message: Message) => void;
+  updateMessage: (id: string, patch: Partial<Message>) => void;
+  removeMessage: (id: string) => void;
   clearMessages: () => void;
 }
 
@@ -14,5 +16,11 @@ export const useMessageStore = create<MessageState>((set) => ({
   setMessages: (messages) => set({ messages }),
   addMessage: (message) =>
     set((state) => ({ messages: [...state.messages, message] })),
+  updateMessage: (id, patch) =>
+    set((state) => ({
+      messages: state.messages.map((m) => (m.id === id ? { ...m, ...patch } : m)),
+    })),
+  removeMessage: (id) =>
+    set((state) => ({ messages: state.messages.filter((m) => m.id !== id) })),
   clearMessages: () => set({ messages: [] }),
 }));
